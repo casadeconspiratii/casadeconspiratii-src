d7c5f421d9a3f1b0ba71fdadb3b5598362b572a5
casadeconspiratii <casadeconspiratii123@gmail.com>
2017-09-18 19:49:32 +0300

    Added partial support for mobile(responsive)

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-    $('html, body').mousewheel(function(e, delta) {
-        this.scrollLeft -= (delta);
-        e.preventDefault();
-    });
+
+	if ($(window).width() >= 768) {
+		$('html, body').mousewheel(function(e, delta) {
+			this.scrollLeft -= (delta);
+			e.preventDefault();
+		});
+	}
+	else {
+		return false;
+	}
+
 });
