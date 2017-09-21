469bafec46f56a903f16fc2d330f7b7cd1179e01
casadeconspiratii <casadeconspiratii123@gmail.com>
2017-09-21 21:56:26 +0300

    Added hero

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
 		return false;
 	}
 
+	$(".profile__koleroo").hover(function(){
+    $(".profile__vifor--bio").css("opacity", "1");
+    $(".profile__vifor img").css("opacity", ".2");
+  }, function() {
+    $(".profile__vifor--bio").css("opacity", "0");
+    $(".profile__vifor img").css("opacity", "1");
+  });
+
+  $(".profile__vifor").hover(function(){
+    $(".profile__koleroo--bio").css("opacity", "1");
+    $(".profile__koleroo img").css("opacity", ".2");
+  }, function() {
+    $(".profile__koleroo--bio").css("opacity", "0");
+    $(".profile__koleroo img").css("opacity", "1");
+  });
+
 });
