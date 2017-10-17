e2e516c45a3750895ae07389b2a26d85699977fc
casadeconspiratii <casadeconspiratii123@gmail.com>
2017-10-17 20:30:19 +0300

    Removed redundant code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,21 +9,4 @@ $(document).ready(function() {
 	else {
 		return false;
 	}
-
-	$(".profile__koleroo").hover(function(){
-    $(".profile__vifor--bio").css("opacity", "1");
-    $(".profile__vifor img").css("opacity", ".2");
-  }, function() {
-    $(".profile__vifor--bio").css("opacity", "0");
-    $(".profile__vifor img").css("opacity", "1");
-  });
-
-  $(".profile__vifor").hover(function(){
-    $(".profile__koleroo--bio").css("opacity", "1");
-    $(".profile__koleroo img").css("opacity", ".2");
-  }, function() {
-    $(".profile__koleroo--bio").css("opacity", "0");
-    $(".profile__koleroo img").css("opacity", "1");
-  });
-
 });
